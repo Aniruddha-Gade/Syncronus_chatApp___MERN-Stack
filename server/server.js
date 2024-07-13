5b7f6fafb3dfab0eab4a507f798c3a9d6b57de50
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-07-13 12:11:59 +0530

    setup Express server with MongoDB connection, middleware, and default route

--- server/server.js
+++ server/server.js
@@ -1,1 +1,44 @@
 
+import express from 'express'
+import dontenv from 'dotenv'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import connectDB from './config/database.js';
+
+dontenv.config() 
+
+const PORT = process.env.PORT || 5000;
+const app = express()
+
+// middleware 
+app.use(express.json()); // to parse json body
+app.use(cookieParser());
+app.use(
+    cors({
+        origin: [process.env.ORIGIN], // frontend link
+        methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH'],
+        credentials: true
+    })
+);
+
+
+
+
+const server = app.listen(PORT, () => {
+    console.log(`Server Started on PORT ${PORT}`);
+});
+
+
+// connect Database
+connectDB()
+
+
+
+// Default Route
+app.get('/', (req, res) => {
+    // console.log('Your server is up and running..!');
+    res.send(`<div>
+    This is Default Route  
+    <p>Everything is OK 😉👍 </p>
+    </div>`);
+})
